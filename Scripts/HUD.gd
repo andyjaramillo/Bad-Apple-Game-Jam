04e331b7989cd476b836ee2bbad205d6b17f2cf6
Andreas Jaramillo <jaramilloan@lancastercountryday.org>
2024-01-21 15:43:28 -0500

    final commit

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -4,16 +4,18 @@ signal can_move
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.start()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Label.text = "Start in " + str(int($Timer.time_left) + 1) 
+	$AnimatedSprite2D.play()
+	if(int($Timer.time_left) == 0):
+		hide()
 	pass
 
 
 func _on_timer_timeout():
 	emit_signal("can_move")
-	hide()
 	pass # Replace with function body.
